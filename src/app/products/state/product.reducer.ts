71f003d3f2647468226045a2ae19f96169896c20
ah-med <ahmedomosanya@gmail.com>
2020-02-25 11:51:05 +0100

    building selectors

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,5 +1,6 @@
 import { Product } from '../product';
 import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export interface State extends fromRoot.State {
     products: ProductState
@@ -15,7 +16,24 @@ const initialState: ProductState = {
     showProductCode: false,
     currentProduct: null,
     products: []
-}; 
+};
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+    getProductFeatureState,
+    state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+    getProductFeatureState,
+    state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+    getProductFeatureState,
+    state => state.products
+);
 
 export function reducer(state = initialState, action): ProductState {
     console.log('THE Incoming Action', action);
